4da95a93a29ddf847261a2c0d0c8e3ff78c2cd26
uday919 <udaykiran919p@gmail.com>
2024-06-29 10:33:17 -0500

    Web Scraping using MultiThreading

---
+++ Multi_threading.py
@@ -0,0 +1,20 @@
+import threading
+import requests
+from bs4 import BeautifulSoup
+urls=[
+    'https://langchain-ai.github.io/langgraph/',
+    'https://python.langchain.com/v0.2/docs/introduction/'
+    
+]
+def fetch_content(url):
+    response=requests.get(url)
+    soup=BeautifulSoup(response.content,'html.parser')
+    print(f'Fetched {len(soup.text)} characters from this {url}')
+threads=[]
+for url in urls:
+    thread=threading.Thread(target=fetch_content,args=(url,))
+    threads.append(thread)
+    thread.start()
+for thread in threads:
+    thread.join()
+print("All webpages fetched")
